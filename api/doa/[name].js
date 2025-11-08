45452e5600ad6634650626a2f2b413e88860b9ab
indratdev <indrat.dev@gmail.com>
2025-11-08 20:38:01 +0700

    update api

---
+++ api/doa/[name].js
@@ -0,0 +1,21 @@
+import doa from '../../data/doa.json';
+
+export default function handler(req, res) {
+  const { name } = req.query;
+  const result = doa.find(d => d.name.toLowerCase().replace(/\s+/g, '-') === name.toLowerCase());
+
+  if (!result) {
+    return res.status(404).json({
+      code: 404,
+      message: "Doa not found",
+      error: true
+    });
+  }
+
+  res.status(200).json({
+    code: 200,
+    message: "Success get doa",
+    data: result,
+    error: false
+  });
+}
