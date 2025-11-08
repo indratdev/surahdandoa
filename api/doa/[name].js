b93efe56151c84bf11c97e5817ef02f270ffd25a
indratdev <indrat.dev@gmail.com>
2025-11-08 20:58:41 +0700

    update api

--- api/doa/[name].js
+++ api/doa/[name].js
@@ -3,8 +3,9 @@ import doa from '../../data/doa.json';
 export default function handler(req, res) {
   const { name } = req.query;
 
-  // cari slug yang cocok, tanpa peduli huruf besar/kecil
-  const result = doa.find(d => d.slug.toLowerCase() === name.toLowerCase());
+  const result = doa.find(
+    d => d.slug.toLowerCase() === name.toLowerCase()
+  );
 
   if (!result) {
     return res.status(404).json({
